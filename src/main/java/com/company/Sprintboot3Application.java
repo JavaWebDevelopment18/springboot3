5e549cb8fab1828ad2ea43dc036e7480b80978d2
Saniya Godil <saniyagodil@gmail.com>
2018-01-29 13:10:44 -0500

    First commit

---
+++ src/main/java/com/company/Sprintboot3Application.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprintboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprintboot3Application.class, args);
+	}
+}
